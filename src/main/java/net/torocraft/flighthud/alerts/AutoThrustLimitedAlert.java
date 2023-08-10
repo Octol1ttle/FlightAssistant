5986df4eb5b9d6f70f8a8a37872b596661a4af80
Octol1ttle <l1ttleofficial@outlook.com>
2023-08-10 17:11:50 +0500

    forgor
    Signed-off-by: Octol1ttle <l1ttleofficial@outlook.com>

--- src/main/java/net/torocraft/flighthud/alerts/AutoThrustLimitedAlert.java
+++ src/main/java/net/torocraft/flighthud/alerts/AutoThrustLimitedAlert.java
@@ -17,7 +17,7 @@ public class AutoThrustLimitedAlert extends Alert {
     @Override
     public int drawText(MinecraftClient mc, DrawContext context, float x, float y, boolean highlight) {
         int i = drawCaution(mc, context, x, y, highlight, "AUTO FLT A/THR LIMITED");
-        i += HudComponent.drawFont(mc, context, " -FRWKS: SELECT", x, y + 10, CONFIG.adviceColor);
+        i += HudComponent.drawFont(mc, context, " -FRWKS: SELECT", x, y + 9, CONFIG.adviceColor);
         return i;
     }
 }
