bf7570788fe655e3dc2e0550d48c6ab28ffac134
Octol1ttle <l1ttleofficial@outlook.com>
2023-08-10 16:48:30 +0500

    Fix thrust lock protection not working on servers
    Signed-off-by: Octol1ttle <l1ttleofficial@outlook.com>

--- src/main/java/net/torocraft/flighthud/mixin/LivingEntityMixin.java
+++ src/main/java/net/torocraft/flighthud/mixin/LivingEntityMixin.java
@@ -1,5 +1,6 @@
 package net.torocraft.flighthud.mixin;
 
+import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.damage.DamageTypes;
@@ -7,13 +8,14 @@ import net.torocraft.flighthud.FlightSafetyMonitor;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(LivingEntity.class)
 public class LivingEntityMixin {
-    @Inject(method = "damage", at = @At("HEAD"))
-    public void detectWallCollision(DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
-        if (source.isOf(DamageTypes.FLY_INTO_WALL))
+    @Inject(method = "onDamaged", at = @At("HEAD"))
+    public void detectWallCollision(DamageSource damageSource, CallbackInfo ci) {
+        LivingEntity $this = (LivingEntity) (Object) this;
+        if ($this instanceof ClientPlayerEntity && damageSource.isOf(DamageTypes.FLY_INTO_WALL))
             FlightSafetyMonitor.thrustLocked = true;
     }
 }
