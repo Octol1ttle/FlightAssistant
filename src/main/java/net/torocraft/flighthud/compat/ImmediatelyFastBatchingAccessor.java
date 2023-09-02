5d1e8fbb596df190e9b742cc7b087b3d557f2749
Octol1ttle <l1ttleofficial@outlook.com>
2023-09-02 13:52:18 +0500

    Fix compatibility with new ImmediatelyFast
    Signed-off-by: Octol1ttle <l1ttleofficial@outlook.com>

--- src/main/java/net/torocraft/flighthud/compat/ImmediatelyFastBatchingAccessor.java
+++ src/main/java/net/torocraft/flighthud/compat/ImmediatelyFastBatchingAccessor.java
@@ -2,10 +2,10 @@ package net.torocraft.flighthud.compat;
 
 public class ImmediatelyFastBatchingAccessor {
     public static void beginHudBatching() {
-        net.raphimc.immediatelyfast.feature.batching.BatchingBuffers.beginHudBatching();
+        net.raphimc.immediatelyfastapi.ImmediatelyFastApi.getApiImpl().getBatching().beginHudBatching();
     }
 
     public static void endHudBatching() {
-        net.raphimc.immediatelyfast.feature.batching.BatchingBuffers.endHudBatching();
+        net.raphimc.immediatelyfastapi.ImmediatelyFastApi.getApiImpl().getBatching().endHudBatching();
     }
 }
