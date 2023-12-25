11cbac0ed6069bafd7af4c7f85a36aaa05e51b4c
Octol1ttle <l1ttleofficial@outlook.com>
2023-12-25 15:16:41 +0500

    fixup! refactor: command registration
    Signed-off-by: Octol1ttle <l1ttleofficial@outlook.com>

--- src/main/java/ru/octol1ttle/flightassistant/commands/SwitchDisplayModeCommand.java
+++ src/main/java/ru/octol1ttle/flightassistant/commands/SwitchDisplayModeCommand.java
@@ -1,14 +1,18 @@
 package ru.octol1ttle.flightassistant.commands;
 
-import com.mojang.brigadier.Command;
-import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
 import ru.octol1ttle.flightassistant.FlightAssistant;
 
-public class SwitchDisplayModeCommand implements Command<FabricClientCommandSource> {
-    @Override
-    public int run(CommandContext<FabricClientCommandSource> context) {
-        FlightAssistant.CONFIG_SETTINGS.toggleDisplayMode(context.getSource().getClient());
-        return 0;
+import static net.fabricmc.fabric.api.client.command.v2.ClientCommandManager.literal;
+
+public class SwitchDisplayModeCommand {
+    public static void register(LiteralArgumentBuilder<FabricClientCommandSource> builder) {
+        builder.then(literal("toggle").executes(
+                context -> {
+                    FlightAssistant.CONFIG_SETTINGS.toggleDisplayMode(context.getSource().getClient());
+                    return 0;
+                }
+        ));
     }
 }
