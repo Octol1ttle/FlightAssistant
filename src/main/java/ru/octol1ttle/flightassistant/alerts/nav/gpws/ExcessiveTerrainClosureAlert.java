28f013e5ba7157ab54fd2ac870f30d193a566fe9
Octol1ttle <l1ttleofficial@outlook.com>
2024-01-13 15:07:23 +0500

    fix: increase reliability of TERRAIN AHEAD alerts
    Signed-off-by: Octol1ttle <l1ttleofficial@outlook.com>

--- src/main/java/ru/octol1ttle/flightassistant/alerts/nav/gpws/ExcessiveTerrainClosureAlert.java
+++ src/main/java/ru/octol1ttle/flightassistant/alerts/nav/gpws/ExcessiveTerrainClosureAlert.java
@@ -3,25 +3,19 @@ package ru.octol1ttle.flightassistant.alerts.nav.gpws;
 import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.text.Text;
-import net.minecraft.util.math.MathHelper;
 import org.jetbrains.annotations.NotNull;
 import ru.octol1ttle.flightassistant.HudComponent;
 import ru.octol1ttle.flightassistant.alerts.AbstractAlert;
 import ru.octol1ttle.flightassistant.alerts.AlertSoundData;
 import ru.octol1ttle.flightassistant.computers.safety.GPWSComputer;
 
-import static net.minecraft.SharedConstants.TICKS_PER_SECOND;
 import static ru.octol1ttle.flightassistant.HudComponent.CONFIG;
 
 public class ExcessiveTerrainClosureAlert extends AbstractAlert {
     private static final float TERRAIN_THRESHOLD = 7.5f;
 
     private static final float PULL_UP_THRESHOLD = 5.0f;
-    private static final float SECONDS_PER_TICK = 1.0f / TICKS_PER_SECOND;
-    private static final float DELAY_ALERT_FOR = 0.5f;
     private final GPWSComputer gpws;
-    private boolean delayFull = false;
-    private float delay = 0.0f;
 
     public ExcessiveTerrainClosureAlert(GPWSComputer gpws) {
         this.gpws = gpws;
@@ -29,21 +23,7 @@ public class ExcessiveTerrainClosureAlert extends AbstractAlert {
 
     @Override
     public boolean isTriggered() {
-        boolean triggered = gpws.descentImpactTime < 0.0f && gpws.terrainImpactTime >= 0.0f;
-        if (triggered) {
-            delay = MathHelper.clamp(delay + SECONDS_PER_TICK, 0.0f, DELAY_ALERT_FOR);
-        } else {
-            delay = MathHelper.clamp(delay - SECONDS_PER_TICK, 0.0f, DELAY_ALERT_FOR);
-        }
-
-        if (delay >= DELAY_ALERT_FOR) {
-            delayFull = true;
-        }
-        if (delay <= 0.0f) {
-            delayFull = false;
-        }
-
-        return delayFull;
+        return gpws.descentImpactTime < 0.0f && gpws.terrainImpactTime >= 0.0f;
     }
 
     @Override
