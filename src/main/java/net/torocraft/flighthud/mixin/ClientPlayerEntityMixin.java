e0aa27415ec152aa7cdb743183d2fa80fb843926
Zomabies <zomabies@users.noreply.github.com>
2024-02-18 01:16:54 +0800

    Fix thrust locking not triggered when collided wall

--- src/main/java/net/torocraft/flighthud/mixin/ClientPlayerEntityMixin.java
+++ src/main/java/net/torocraft/flighthud/mixin/ClientPlayerEntityMixin.java
@@ -9,12 +9,17 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(LivingEntity.class)
-public class LivingEntityMixin {
+@Mixin(ClientPlayerEntity.class)
+public class ClientPlayerEntityMixin {
     @Inject(method = "damage", at = @At("HEAD"))
     public void detectWallCollision(DamageSource damageSource, float amount, CallbackInfoReturnable<Boolean> cir) {
         LivingEntity $this = (LivingEntity) (Object) this;
-        if ($this instanceof ClientPlayerEntity && damageSource == DamageSource.FLY_INTO_WALL) {
+        if ($this instanceof ClientPlayerEntity
+                && $this.isFallFlying()
+                // Damage source is always generic except fall damage in 1.19.2
+                // Use bounding box to check for actual collision
+                && (damageSource == DamageSource.FLY_INTO_WALL
+                    || $this.getWorld().canCollide($this, $this.getBoundingBox().expand(3)))) {
             FlightSafetyMonitor.thrustLocked = true;
         }
     }
