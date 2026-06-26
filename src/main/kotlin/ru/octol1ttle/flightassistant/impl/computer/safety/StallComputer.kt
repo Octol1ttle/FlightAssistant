0a8c74ecaaa4138cf83c0bd79de31bc50d8dfc24
Octol1ttle <l1ttleofficial@outlook.com>
2026-06-26 12:51:57 +0500

    fix stall limit arrows having wrong color

--- src/main/kotlin/ru/octol1ttle/flightassistant/impl/computer/safety/StallComputer.kt
+++ src/main/kotlin/ru/octol1ttle/flightassistant/impl/computer/safety/StallComputer.kt
@@ -36,9 +36,9 @@ class StallComputer(computers: ComputerBus) : Computer(computers) {
             ))
         }
 
-        if (query is PitchComputer.MaximumPitchQuery && maximumSafePitch <= 90.0f && !computers.data.fallDistanceSafe) {
+        if (query is PitchComputer.MaximumPitchQuery && !computers.data.fallDistanceSafe) {
             query.respond(ControlInput(
-                maximumSafePitch - 5.0f,
+                (maximumSafePitch - 5.0f).coerceAtMost(90.0f),
                 Component.translatable("mode.flightassistant.vertical.stall_protection"),
                 ControlInput.Priority.HIGHEST,
                 1.5f,
